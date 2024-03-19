76742107bf9d83d1e9c93ed9254b6a8f07e20641
nveles <159052126+nveles@users.noreply.github.com>
2024-03-19 17:18:56 -0600

    up

---
+++ Lab4/Lab4/story_content/user.js
@@ -0,0 +1,59 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5aCgnrjL2qs":
+        Script1();
+        break;
+      case "6XpJnpcM1V2":
+        Script2();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  const target = object('6D3qSUXWR1W');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6drhpKYmgVQ';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+window.Script2 = function()
+{
+  const target = object('6JkplkMTkhM');
+const duration = 750;
+const easing = 'ease-out';
+const id = '6csNtUZYNTp';
+const pulseAmount = 0.07;
+player.addForTriggers(
+id,
+target.animate([
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }, { scale: `${1 + pulseAmount}` },
+{ scale: '1' }
+],
+  { fill: 'forwards', duration, easing }
+)
+);
+}
+
+};
